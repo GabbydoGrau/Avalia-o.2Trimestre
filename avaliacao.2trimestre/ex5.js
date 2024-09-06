11325569bbfea08f3c5030c3011a56b273d624d1
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-06 12:12:02 -0300

    ex5.js

---
+++ avaliacao.2trimestre/ex5.js
@@ -0,0 +1,11 @@
+//media de 3 notas com media ponderada de 2,3 e 5,0
+
+let nota1 = 4,0;
+let nota2 = 5,0;
+let nota3 = 2,5;
+const soma = (nota1 + nota2 + nota3) \5; //ou por 3, pesquisa depois isso ai 
+
+console.log("a nota final é: " + soma); 
+if soma = < 5,0 
+print ("Aprovado!");
+else print ("Reprovado") //pesquisar o print sem console.

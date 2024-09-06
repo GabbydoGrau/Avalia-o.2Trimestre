7748b9b9b1eec763b256be8845752a79e4f2b863
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-06 12:20:42 -0300

    ex6.js

---
+++ avaliacao.2trimestre/ex6.js
@@ -0,0 +1,7 @@
+//tempo de duração em horas minutos e segundos 
+var segundos = 60000;
+console.log(segundos);
+var horas = segundos/60;
+console.log (horas);
+var minutos = segundos/60;
+console.log(minutos); //revisa pra caralho essa merda aqui viu, depois pesquisa mais um monte.

c45fba9e817c1dc1a98afcbc619a9bec105173ac
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-06 12:21:46 -0300

    ex1.js

--- avaliacao.2trimestre/ex1.js
+++ avaliacao.2trimestre/ex1.js
@@ -6,4 +6,4 @@ let y1= 2;
 let y2= 10;
 const conta= (x2-x1)**2 + (y2-y1)**2 ;
 var d = conta
-console.log("d = " + conta);
+console.log("d = " + conta); //faltou dividir a variavel conta, pesquisa mais sobre depois, e revisa pra caralho.

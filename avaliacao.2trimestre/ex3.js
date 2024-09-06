c0d4914081576f675b4f16171096758addea56cf
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-06 08:19:35 -0300

    ex3.js

---
+++ avaliacao.2trimestre/ex3.js
@@ -0,0 +1,8 @@
+//idade em anos, meses e dias, mas só printada em dias 
+
+var dia = ("Digite os dias: ");
+var mes = ("Digite os meses: ");
+var ano = ("Digite os anos: ");
+
+var soma = dia + mes + ano;
+console.log("Idade em dias: " + soma);

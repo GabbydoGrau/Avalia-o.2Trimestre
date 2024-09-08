cf87145a24ac7affa52a3f8b4df318eea3d3a7b9
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-08 20:15:44 -0300

    ex8.js

--- avaliacao.2trimestre/ex8.js
+++ avaliacao.2trimestre/ex8.js
@@ -1,1 +1,9 @@
-
+function coeficientes(A, B, C, D, E, F) 
+  
+    const denominador = (A * E) - (B * D)
+    const x = (c * e - b * f) / denominador
+    const y = (a * f - c * d) / denominador
+  
+    console.log("O valor de x é: " + x);
+    console.log("O valor de y é: " + y);
+}

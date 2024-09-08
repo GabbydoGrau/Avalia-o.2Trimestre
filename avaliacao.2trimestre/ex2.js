db3642b1b0c65c0a5af7628f1098c3cb3113e6af
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-08 19:25:53 -0300

    ex2.js

--- avaliacao.2trimestre/ex2.js
+++ avaliacao.2trimestre/ex2.js
@@ -1,1 +1,13 @@
-// conta e mais conta
+// ler 3 numeros inteiros...
+function lerNumero (A , B, C) { // a=3, b=2, c=4
+  if (A <= 0 || B <= 0 || C <= 0) {
+  }
+    let r = (3 + 2, 2);   //lembrar de math.pow
+    let s = (B + C, 2);   //precisa de math.pow?
+
+ 
+    let d = (r + s) / 2;
+
+
+    console.log("D : ", + d);
+}

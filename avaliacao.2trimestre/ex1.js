c981223316cf685cb5d9f450c8bc2ea12744aec9
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-08 19:06:15 -0300

    ex1.js

--- avaliacao.2trimestre/ex1.js
+++ avaliacao.2trimestre/ex1.js
@@ -1,9 +1,10 @@
-//contruir um algoritimo
+//construa um algoritmo que...
 
 let x1= 3;
 let x2= 7;
 let y1= 2;
 let y2= 10;
-const conta= (x2-x1)**2 + (y2-y1)**2 ;
-var d = conta
-console.log("d = " + conta); //faltou dividir a variavel conta, pesquisa mais sobre depois, e revisa pra caralho.
+const conta = (x2-x1)**2 + (y2-y1)**2 ;
+const distancia = conta /2;
+
+console.log("d = " + conta); //pesquisar sobre math.sqrt e entender 

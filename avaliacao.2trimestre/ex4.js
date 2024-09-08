14e598e27ff357de4c7874ef812a8cbb0602e1b4
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-08 20:04:41 -0300

    ex4.js

--- avaliacao.2trimestre/ex4.js
+++ avaliacao.2trimestre/ex4.js
@@ -1,1 +1,12 @@
 //idade em dias, mostra em anos, meses e dias 
+const ano = 43
+const mese = 12
+const dia = 365
+const mesporano = 12
+const diapormes = 30
+let anos = ano * mesporano
+let meses = dia / diapormes
+let dias = mese * diapormes
+console.log("Idade em dias é:", + dias)
+console.log("Idade em meses é: " + meses)
+console.log("Idade em anos é: " anos)

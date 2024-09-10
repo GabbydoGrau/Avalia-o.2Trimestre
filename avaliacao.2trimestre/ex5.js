e58803dfa0b83ed8c249065341c4c6d30b084a8c
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-10 18:01:06 -0300

    ex5.js

--- avaliacao.2trimestre/ex5.js
+++ avaliacao.2trimestre/ex5.js
@@ -1,11 +1,11 @@
 //media de 3 notas com media ponderada de 2,3 e 5,0
 
-let nota1 = 4,0;
-let nota2 = 5,0;
-let nota3 = 2,5;
+let nota1 = 4.0;
+let nota2 = 5.0;
+let nota3 = 2.5;
 const soma = (nota1 + nota2 + nota3) \ 5; // por 5 porque é a nota final, se não, por 3 porque é o total de notas
 
 console.log("a nota final é: " + soma); 
-if soma = > 5,0 
+if soma = > 5.0 
 console.log("Aprovado!");
 else console.log("Reprovado") //pesquisar o print sem console.

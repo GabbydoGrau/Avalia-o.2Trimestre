f383086134fd9ec705ff4113d8b189a928050e5d
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-09-24 09:01:35 -0300

    ex1.js

--- avaliacao.2trimestre/ex1.js
+++ avaliacao.2trimestre/ex1.js
@@ -4,7 +4,7 @@ let x1= 3;
 let x2= 7;
 let y1= 2;
 let y2= 10;
-const conta = (x2-x1)**2 + (y2-y1)**2 ;
+const conta = (x2-x1)**2 + (y2-y1)**2 ; //professor disse que ** serve pra fazer raiz
 const distancia = conta /2;
 
-console.log("d = " + distancia); //pesquisar sobre math.sqrt e entender 
+console.log("d = " + distancia); //pesquisar sobre math.sqrt e entender // segundo o google, math.sqrt retorna a raiz quadrada de um número
